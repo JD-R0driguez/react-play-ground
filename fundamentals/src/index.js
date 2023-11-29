b5163fa96d41c74be5c121d34d3079185620f741
Juan <jd.rodriguez.or@gmail.com>
2023-11-29 15:48:22 -0500

    First nested compoment

--- fundamentals/src/index.js
+++ fundamentals/src/index.js
@@ -1,10 +1,30 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import Logo from './images/logo-react.svg';
 
-function Greeting() {
-  return <h2>My First Component</h2>;
+
+const MyNav = () =>{
+    return(
+        <nav>
+            <img src={Logo} alt="react logo" width="50px" height="auto"/>
+            <h1>Fun Facts About React</h1>
+            <ul>
+                <li>Component-Based Architecture</li>
+                <li>Virtual DOM for Performance Optimization</li>  
+                <li>Developed by Facebook</li>
+                <li>JSX - JavaScript Syntax Extension</li>
+                <li>React Native for Cross-Platform Development</li>   
+            </ul>
+        </nav>
+    )
 }
+ReactDOM.render(<MyNav/>, document.getElementById('root'));
+// const app_root =  ReactDOM.createRoot(document.getElementById('root'));
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// app_root.render(
+//     <div>
+//         <MyNav/>
+//         {/* <MySecondList/> */}
+//     </div>
+// );
 
-root.render(<Greeting />);
