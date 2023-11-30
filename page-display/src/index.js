79162d7d5ac407917a1b060b71ee052e455b2e31
Juan <jd.rodriguez.or@gmail.com>
2023-11-30 16:55:14 -0500

    Page display initial commit

---
+++ page-display/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+
+function HelloText (){
+    return(
+        <h1>Testing setup</h1>
+    )
+}
+
+ReactDOM.render(<HelloText/>, document.getElementById('root'));
