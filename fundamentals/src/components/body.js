43d587030f2444d08a1d6f15aa35c5a3df8cb1b0
Juan <jd.rodriguez.or@gmail.com>
2023-11-30 16:15:51 -0500

    Separated page into components

---
+++ fundamentals/src/components/body.js
@@ -0,0 +1,25 @@
+import React from "react"
+
+function Body(){
+    return (
+        <body>
+            <h1>Fun Facts About React</h1>
+            <ul className="sub-list">
+            <li>Component-Based Architecture</li>
+            <li>Virtual DOM for Performance Optimization</li>  
+            <li>Developed by Facebook</li>
+            <li>JSX - JavaScript Syntax Extension</li>
+            <li>React Native for Cross-Platform Development</li> 
+        </ul>
+            <h1>Why do I want to lear React</h1>
+            <ul className="sub-list">
+            <li>Enables easier creation of interactive user interfaces.</li>
+            <li>Extensive community support and rich documentation.</li>
+            <li>Enhances career prospects due to its industry prevalence.</li>
+        </ul>
+            
+        </body>
+    )
+}
+
+export default Body;
--- fundamentals/src/index.js
+++ fundamentals/src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './assets/styles.css';
 import Header from './components/Header';
+import Body from './components/body';
 import Footer from './components/Footer';
+import './assets/styles.css';
 
 
 
@@ -11,44 +12,10 @@ const Page = ()=>{
         <>
             <Header/>
             <Body/>
+            <Footer/>
         </>
     )
 }
-
-function Body(){
-    return (
-        <body>
-            <h1>Fun Facts About React</h1>
-            <ReactFacts/>
-            <h1>Why do I want to lear React</h1>
-            <WhyReact/>
-            <Footer/>
-        </body>
-    )
-}
-
-function ReactFacts(){
-    return(
-        <ul className="sub-list">
-            <li>Component-Based Architecture</li>
-            <li>Virtual DOM for Performance Optimization</li>  
-            <li>Developed by Facebook</li>
-            <li>JSX - JavaScript Syntax Extension</li>
-            <li>React Native for Cross-Platform Development</li> 
-        </ul>
-    )
-}
-
-const WhyReact = () =>{
-    return(
-        <ul className="sub-list">
-            <li>Enables easier creation of interactive user interfaces.</li>
-            <li>Extensive community support and rich documentation.</li>
-            <li>Enhances career prospects due to its industry prevalence.</li>
-        </ul>
-    )
-}
-
 
 ReactDOM.render(<Page/>, document.getElementById('root'));
 
