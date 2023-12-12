7082ec98c887628f676bf8163b1aa5a2ca086fc2
Juan <jd.rodriguez.or@gmail.com>
2023-12-12 08:44:07 -0500

    Changed attributes in Card component

--- bnb-clone-page/src/components/Card/Card.js
+++ bnb-clone-page/src/components/Card/Card.js
@@ -1,30 +1,26 @@
 import React from 'react'
 import './Card.css'
+import StarIcon from '../../assets/rating.svg'
 
 export default function Card(props){
-    const rainbowColors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
-
 
     return(
         <div className='card'>
-            
-            <img className="card-image" src={props.img}/>
-            
+            <img className="card-image" src={props.img}/>        
             <div className='text-container'>
                 <div className='top-line'>
-                    <img className="star" src={props.ratingIcon}/>
+                    <img className="star" src={StarIcon}/>
                     {props.rating}
                     <div className='gray-text'>
-                        ({props.revCount}) - {props.country}
+                        ({props.reviewCount}) - {props.location}
                     </div>   
                 </div>
                 {props.subtitle}<p></p>
-                <strong className='price'>Price {props.price}</strong> / {props.people}
+                <strong className='price'>From ${props.price}</strong> / person
             </div>
 
 
         </div>
     )
 }
-
 
