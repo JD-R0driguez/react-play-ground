946ef99c9dde864a0ca089b40d1ca7dc91328e4e
Juan <jd.rodriguez.or@gmail.com>
2023-12-13 11:42:20 -0500

    Updated card component to recieve and item array as props

--- bnb-clone-page/src/components/Card/Card.js
+++ bnb-clone-page/src/components/Card/Card.js
@@ -4,19 +4,24 @@ import StarIcon from '../../assets/rating.svg'
 
 export default function Card(props){
 
+    let badgeText;
+    if (props.item.spotsAvailable === 0) badgeText = "SOLD OUT";
+    else if (props.item.location === "Online") badgeText = "ONLINE";
+
     return(
         <div className='card'>
-            <img className="card-image" src={props.img}/>        
+            <img className="card-image" src={props.item.coverImg}/>
+            {badgeText && <div className='badge'>{badgeText}</div>  }
             <div className='text-container'>
                 <div className='top-line'>
                     <img className="star" src={StarIcon}/>
-                    {props.rating}
+                    {props.item.stats.rating}
                     <div className='gray-text'>
-                        ({props.reviewCount}) - {props.location}
+                        ({props.item.stats.reviewCount}) - {props.item.location}
                     </div>   
                 </div>
-                {props.subtitle}<p></p>
-                <strong className='price'>From ${props.price}</strong> / person
+                {props.item.subtitle}<p></p>
+                <strong className='price'>From ${props.item.price}</strong> / person
             </div>
 
 
