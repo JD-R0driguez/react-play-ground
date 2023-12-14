c19d2d3395898b28c646e5290aa5b6d59da3fe0b
Juan <jd.rodriguez.or@gmail.com>
2023-12-14 18:34:57 -0500

    Added images sources

--- travel-journal/src/placesData.js
+++ travel-journal/src/placesData.js
@@ -1,6 +1,7 @@
 export default [
   {
     title: "Colosseum, Rome",
+    imageSource: "https://plus.unsplash.com/premium_photo-1675975706513-9daba0ec12a8?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     location: "Italy",
     googleMapsUrl: "https://maps.app.goo.gl/ajUedXi4puh2fGeT7",
     startDate: "12 Jan, 2021",
@@ -10,6 +11,7 @@ export default [
   },
   {
     title: "Castello Svevo, Termoli",
+    imageSource: "https://lh3.googleusercontent.com/p/AF1QipMMVdniKl8H3V5yXBaeHNm94EJv3pWakRMKlenw=s1360-w1360-h1020",
     location: "Italy",
     googleMapsUrl: "https://maps.app.goo.gl/vEWmXbuTuWQzTSiB7",
     startDate: "12 Jan, 2021",
@@ -19,6 +21,7 @@ export default [
   },
   {
     title: "San Nicola Island, Tremiti Islands",
+    imageSource:"https://arrivalshall.files.wordpress.com/2021/03/isole-caprara-from-san-domino-tremiti-islands.jpg",
     location: "Italy",
     googleMapsUrl: "https://maps.app.goo.gl/udyPNZGYg5DBQY2w6",
     startDate: "12 Jan, 2021",
@@ -28,6 +31,7 @@ export default [
   },
   {
     title: "Quartieri Spagnoli, Naples",
+    imageSource:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwJedfvJ90CEzIYatDRH54cmriAGwVQRFV4b6S-0_HoEO8hzkAYlFvFTtI0MDcpgmZhTY&usqp=CAU",
     location: "Italy",
     googleMapsUrl: "https://maps.app.goo.gl/PZLGZcMNoeu8NUvcA",
     startDate: "12 Jan, 2021",
@@ -38,6 +42,7 @@ export default [
   },
   {
     title: "La Rambla, Barcelona",
+    imageSource:"https://d1bv4heaa2n05k.cloudfront.net/user-images/1490700991916/shutterstock-323369702_main_1490701004864.jpeg",
     location: "Spain",
     googleMapsUrl: "https://maps.app.goo.gl/5ysFr8tMXyfzEWwK7",
     startDate: "12 Jan, 2021",
@@ -47,6 +52,7 @@ export default [
   },
   {
     title: "Sagrada Familia, Barcelona",
+    imageSource:"https://lh3.googleusercontent.com/p/AF1QipO6QW5xMK10tqffvH8VtHLYQW6SPTJ8wJt0oVy7=s1360-w1360-h1020",
     location: "Spain",
     googleMapsUrl: "https://maps.app.goo.gl/M8h7cjs6k3G5pCTm8",
     startDate: "12 Jan, 2021",
@@ -56,6 +62,7 @@ export default [
   },
   {
     title: "San Lorenzo's Beach, Gijón",
+    imageSource:"https://a.cdn-hotels.com/gdcs/production10/d230/64104660-68ce-11e8-bbf2-0242ac110009.jpg",
     location: "Spain",
     googleMapsUrl: "https://maps.app.goo.gl/o52b7K7Z3bqm3ZNV6",
     startDate: "12 Jan, 2021",
@@ -65,6 +72,7 @@ export default [
   },
   {
     title: "Tower of Hercules, A Coruña",
+    imageSource:"https://galiciaturismorural.es/views/layouts/plantilla/img/recursos/1/torre-de-hercules-NXG1sO.jpg",
     location: "Spain",
     googleMapsUrl: "https://maps.app.goo.gl/Bs2qr9ooCNWMdZv77",
     startDate: "12 Jan, 2021",
@@ -74,6 +82,7 @@ export default [
   },
   {
     title: "Douro River, Porto",
+    imageSource:"https://i0.wp.com/touristjourney.com/wp-content/uploads/2020/10/shutterstock_1706807566-scaled.jpg?fit=2560%2C1707&ssl=1",
     location: "Portugal",
     googleMapsUrl: "https://maps.app.goo.gl/MXqNENUHrYYnVJC66",
     startDate: "12 Jan, 2021",
@@ -84,6 +93,7 @@ export default [
   },
   {
     title: "Tower of Belém, Lisbon",
+    imageSource:"https://lisboacamping.com/wp-content/uploads/2020/03/Sunrise-Torre-Belem-Lisbon-Portugal-2-21-1.jpg",
     location: "Portugal",
     googleMapsUrl: "https://maps.app.goo.gl/JoodJ1chCVBcEUgg7",
     startDate: "12 Jan, 2021",
@@ -93,6 +103,7 @@ export default [
   },
   {
     title: "Praca do Comércio, Lisbon",
+    imageSource:"https://live.staticflickr.com/65535/53360257134_18ca0c9264_c.jpg",
     location: "Portugal",
     googleMapsUrl: "https://maps.app.goo.gl/LQaXfhWRaQ62ArDm7",
     startDate: "12 Jan, 2021",
@@ -102,6 +113,7 @@ export default [
   },
   {
     title: "Setúbal Municipal Market",
+    imageSource:"https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/Mercado_Municipal%2C_Set%C3%BAbal%2C_Portugal%2C_2012-05-11%2C_DD_03.JPG/2560px-Mercado_Municipal%2C_Set%C3%BAbal%2C_Portugal%2C_2012-05-11%2C_DD_03.JPG",
     location: "Portugal",
     googleMapsUrl: "https://maps.app.goo.gl/6iaecc6kiYh1vugX9",
     startDate: "12 Jan, 2021",
@@ -112,6 +124,7 @@ export default [
   },
   {
     title: "Citadela of Cascais",
+    imageSource:"https://www.movingtoportugal.pt/wp-content/uploads/2020/11/Viver_cascais_630_420-2.jpg",
     location: "Portugal",
     googleMapsUrl: "https://maps.app.goo.gl/Gzc3rW4xqQsoXKf99",
     startDate: "12 Jan, 2021",
