b6afe28e20cf6ed399204c020526f8ee1441971e
Juan <jd.rodriguez.or@gmail.com>
2023-12-14 14:52:19 -0500

    Fixed data links and properties

---
+++ travel-journal/src/placesData.js
@@ -0,0 +1,124 @@
+export default [
+  {
+    title: "Colosseum, Rome",
+    location: "Italy",
+    googleMapsUrl: "https://maps.app.goo.gl/ajUedXi4puh2fGeT7",
+    startDate: "12 Jan, 2021",
+    endDate: "24 Jan 2021",
+    description: "The Colosseum, an ancient amphitheater in Rome, was used for gladiatorial contests and public spectacles, " +
+      "and is an iconic symbol of Rome's rich history and architectural prowess."
+  },
+  {
+    title: "Castello Svevo, Termoli",
+    location: "Italy",
+    googleMapsUrl: "https://maps.app.goo.gl/vEWmXbuTuWQzTSiB7",
+    startDate: "12 Jan, 2021",
+    endDate: "24 Jan 2021",
+    description: "The Castello Svevo in Termoli, a historic castle overlooking the Adriatic Sea, " +
+      "stands as a testament to the region's medieval heritage and offers breathtaking views of the coastline."
+  },
+  {
+    title: "San Nicola Island, Tremiti Islands",
+    location: "Italy",
+    googleMapsUrl: "https://maps.app.goo.gl/udyPNZGYg5DBQY2w6",
+    startDate: "12 Jan, 2021",
+    endDate: "24 Jan 2021",
+    description: "San Domino, one of the Tremiti Islands, captivates with its stunning beaches, " +
+      "crystal-clear waters, and caves, providing a serene retreat within a beautiful natural setting."
+  },
+  {
+    title: "Quartieri Spagnoli, Naples",
+    location: "Italy",
+    googleMapsUrl: "https://maps.app.goo.gl/PZLGZcMNoeu8NUvcA",
+    startDate: "12 Jan, 2021",
+    endDate: "24 Jan 2021",
+    description: "The Hispanic Quarter in Naples, also known as Quartieri Spagnoli, " +
+      "is a vibrant and lively neighborhood renowned for its narrow streets, " +
+      "colorful buildings, bustling markets, and authentic Neapolitan atmosphere."
+  },
+  {
+    title: "La Rambla, Barcelona",
+    location: "Spain",
+    googleMapsUrl: "https://maps.app.goo.gl/5ysFr8tMXyfzEWwK7",
+    startDate: "12 Jan, 2021",
+    endDate: "24 Jan 2021",
+    description: "La Rambla, Barcelona's bustling tree-lined street, is renowned for its vibrant atmosphere, " +
+      "street performers, markets, and historic landmarks, making it a must-visit thoroughfare in the city."
+  },
+  {
+    title: "Sagrada Familia, Barcelona",
+    location: "Spain",
+    googleMapsUrl: "https://maps.app.goo.gl/M8h7cjs6k3G5pCTm8",
+    startDate: "12 Jan, 2021",
+    endDate: "24 Jan 2021",
+    description: "The Sagrada Familia, Antoni Gaudí's masterpiece in Barcelona, " +
+      "is an awe-inspiring basilica renowned for its intricate design and spiritual significance."
+  },
+  {
+    title: "San Lorenzo's Beach, Gijón",
+    location: "Spain",
+    googleMapsUrl: "https://maps.app.goo.gl/o52b7K7Z3bqm3ZNV6",
+    startDate: "12 Jan, 2021",
+    endDate: "24 Jan 2021",
+    description: "Gijón's beautiful beaches, such as Playa de San Lorenzo and Poniente Beach, offer golden sands, " +
+      "surfing spots, and a vibrant seaside atmosphere, attracting locals and tourists alike."
+  },
+  {
+    title: "Tower of Hercules, A Coruña",
+    location: "Spain",
+    googleMapsUrl: "https://maps.app.goo.gl/Bs2qr9ooCNWMdZv77",
+    startDate: "12 Jan, 2021",
+    endDate: "24 Jan 2021",
+    description: "The Tower of Hercules in A Coruña, an ancient Roman lighthouse, " +
+      "is the oldest working lighthouse in the world and an emblem of the city's maritime history."
+  },
+  {
+    title: "Douro River, Porto",
+    location: "Portugal",
+    googleMapsUrl: "https://maps.app.goo.gl/MXqNENUHrYYnVJC66",
+    startDate: "12 Jan, 2021",
+    endDate: "24 Jan 2021",
+    description: "The Douro River in Porto, a UNESCO World Heritage site, " +
+      "is famous for its stunning landscapes, picturesque vineyards, " +
+      "and the traditional Rabelo boats, offering delightful cruises and scenic beauty along its banks."
+  },
+  {
+    title: "Tower of Belém, Lisbon",
+    location: "Portugal",
+    googleMapsUrl: "https://maps.app.goo.gl/JoodJ1chCVBcEUgg7",
+    startDate: "12 Jan, 2021",
+    endDate: "24 Jan 2021",
+    description: "The Tower of Belém, a UNESCO World Heritage site in Lisbon, " +
+      "is a striking example of Manueline architecture and played a significant role in Portugal's maritime history."
+  },
+  {
+    title: "Praca do Comércio, Lisbon",
+    location: "Portugal",
+    googleMapsUrl: "https://maps.app.goo.gl/LQaXfhWRaQ62ArDm7",
+    startDate: "12 Jan, 2021",
+    endDate: "24 Jan 2021",
+    description: "Praca do Comércio, Lisbon's grand riverside square, showcases impressive architecture, " +
+      "historical significance, and offers stunning views of the Tagus River, serving as a focal point of the city."
+  },
+  {
+    title: "Setúbal Municipal Market",
+    location: "Portugal",
+    googleMapsUrl: "https://maps.app.goo.gl/6iaecc6kiYh1vugX9",
+    startDate: "12 Jan, 2021",
+    endDate: "24 Jan 2021",
+    description: "The Setúbal Municipal Market is a lively and traditional market " +
+      "where locals and visitors can find a wide array of fresh produce, fish, and regional delicacies, " +
+      "reflecting the city's rich gastronomic heritage."
+  },
+  {
+    title: "Citadela of Cascais",
+    location: "Portugal",
+    googleMapsUrl: "https://maps.app.goo.gl/Gzc3rW4xqQsoXKf99",
+    startDate: "12 Jan, 2021",
+    endDate: "24 Jan 2021",
+    description: "Cascais, renowned for its stunning coastline, boasts beautiful beaches like Praia do Guincho and " +
+      "Praia da Rainha, offering golden sands and excellent opportunities for water sports."
+  }
+];
+
+  
