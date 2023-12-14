0ab9ffb5efdca5fe15ffd391b781de5a74f5e565
Juan <jd.rodriguez.or@gmail.com>
2023-12-14 14:54:03 -0500

    Added props to card component and linked to its instances in App.js

--- travel-journal/src/App.js
+++ travel-journal/src/App.js
@@ -1,9 +1,21 @@
 import React from "react";
 import Header from "./components/Header/Header"
+import Card from "./components/Card/Card"
+import placesData from "./placesData"
 
 
 export default function App(){
+    
+    const arrayOfPlaces = placesData.map(place =>(
+        <Card
+            {...place}
+        />
+    ));
     return(
-        <Header/>
+        <div>
+            <Header/>
+            {arrayOfPlaces}
+        </div>
+    
     )
 }
---
+++ travel-journal/src/components/Card/Card.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+
+
+export default function Card(props){
+    return(
+        <div className="card-container">
+            <div className="image-container">
+                <img className="card-photo" src="https://source.unsplash.com/WLxQvbMyfas" width="125px" height="168" alt=""/>
+            </div>
+            <div className="text-container">
+                <div className="location-text"><h2>{props.location}</h2></div>
+                <a className="google-location-text" href={props.googleMapsUrl}>View on Google Maps</a>
+                <h1 className="place-name">{props.title}</h1>
+                <h1 className="travel-dates">{props.startDate}{props.endDate}</h1>
+                <h3 className="place-info">{props.description}</h3>
+            </div>
+        </div>
+    )
+}
