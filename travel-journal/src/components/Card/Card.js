801ff45f39428e91c354db62761752e1167745ae
Juan <jd.rodriguez.or@gmail.com>
2023-12-15 11:39:43 -0500

    Added grid display to body and styles to text

--- travel-journal/src/components/Card/Card.js
+++ travel-journal/src/components/Card/Card.js
@@ -1,4 +1,6 @@
 import React from "react";
+import locationIcon from "../../assets/location-icon.svg"
+import "./Card.css"
 
 
 
@@ -6,11 +8,14 @@ export default function Card(props){
     return(
         <div className="card-container">
             <div className="image-container">
-                <img className="card-photo" src={props.imageSource} width="125px" height="168" alt=""/>
+                <img className="photo-card" src={props.imageSource}/>
             </div>
             <div className="text-container">
-                <div className="location-text"><h2>{props.location}</h2></div>
-                <a className="google-location-text" href={props.googleMapsUrl}>View on Google Maps</a>
+                <div className="location-text">
+                    <img className="location-icon" src={locationIcon}/>
+                    <h2>{props.location}</h2>
+                    <a className="google-location-text" href={props.googleMapsUrl}>View on Google Maps</a>
+                </div>
                 <h1 className="place-name">{props.title}</h1>
                 <h1 className="travel-dates">{props.startDate}{props.endDate}</h1>
                 <h3 className="place-info">{props.description}</h3>
