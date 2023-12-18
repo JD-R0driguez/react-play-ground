1d3388d855497fc122419c901d5cbf5c53e0e409
Juan <jd.rodriguez.or@gmail.com>
2023-12-18 16:42:13 -0500

    Included Meme component into App

--- api-mem-generator/src/App.js
+++ api-mem-generator/src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
 import Header from './components/Header'
+import MemeBox from './components/Meme'
 
 function App(){
     return (
-        <Header />
+        <div>            
+            <Header />
+            <MemeBox />
+        </div>
     )
 }
 
