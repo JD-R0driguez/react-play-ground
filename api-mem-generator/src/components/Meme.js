3a76cadbdc069a9794a092aa1b461599c7e75c9f
Juan <jd.rodriguez.or@gmail.com>
2023-12-19 15:53:47 -0500

    Added state to Meme component to update image when clicking

--- api-mem-generator/src/components/Meme.js
+++ api-mem-generator/src/components/Meme.js
@@ -1,17 +1,34 @@
 import React from "react";
+import memesData from '../memesData'
 import './Meme.css'
 
 
 export default function Meme(){
+    
+    const [memeImage, setMemeImage] = React.useState();
+
+    function handleClick(){
+        const memesArray = memesData.data.memes;
+        const randomIndex = Math.floor(Math.random() * memesArray.length);
+        const randomMemeUrl =  memesArray[randomIndex].url;
+
+        setMemeImage(image => randomMemeUrl)
+    }
+
     return (
         <main>
-            <form id="meme-form">
+            <div id="meme-form">
 
-                <input className="box-input" type="text" id="top-input" name="top-input" placeholder="Shut Up"/>
-                <input className="box-input" type="text" id="bottom-input" name="bottom-input" placeholder="And take my money"/>
+                <input className="box-input" type="text" id="top-input" name="top-input" placeholder="Top text"/>
+                <input className="box-input" type="text" id="bottom-input" name="bottom-input" placeholder="Bottom text"/>
 
-                <button className="submit-button" type="submit">Get a new meme image  🖼</button>
-                </form>
+                <button 
+                    onClick={handleClick}
+                    className="submit-button" 
+                    type="submit">Get a new meme image  🖼
+                </button>
+            </div>
+            <img src={memeImage} width="300px"/>
         </main>
     )
 }
