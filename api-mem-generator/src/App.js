2027dc4a5166a41cd71661e53c7cc4de5c1c72d5
Juan <jd.rodriguez.or@gmail.com>
2024-01-17 13:27:53 -0500

    Simple counter using state practice

--- api-mem-generator/src/App.js
+++ api-mem-generator/src/App.js
@@ -1,12 +1,21 @@
 import React from "react";
 import Header from './components/Header'
 import MemeBox from './components/Meme'
+import './styles.css'
 
 function App(){
     return (
         <div>            
             <Header />
-            <MemeBox />
+
+            <div className="counter">
+                <button className="counter--minus">-</button>
+                <div className="counter--count">
+                    <h1>0</h1>
+                </div>
+                <button className="counter--plus">+</button>
+            </div>
+            {/* <MemeBox /> */}
         </div>
     )
 }
