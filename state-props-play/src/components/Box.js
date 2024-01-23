5eec77cf10e802dc2053c177afcbeb62a10c9683
Juan <jd.rodriguez.or@gmail.com>
2024-01-23 13:38:50 -0500

    Third challenge completed

--- state-props-play/src/components/Box.js
+++ state-props-play/src/components/Box.js
@@ -2,14 +2,26 @@ import React from 'react';
 
 function Box(props){
 
-    const colorBackground = props.state? "#222222": "none"; 
+    function handleClick(){
+        //Toggle the box when clicked
+        setBoxState(prevState => !prevState)
+    }
+
+    //State to manage te box's state (clicked / un-clicked)
+    const [boxState, setBoxState] = React.useState(props.state)
+
+    //Determine the back color based on the passed value from props
+    const colorBackground = boxState? "#222222": "transparent"; 
     
     const styles = {
         backgroundColor: colorBackground
     }
 
     return (
-        <div style={styles} className="box"></div>
+        <button className='button--box' onClick={handleClick}>
+            <div style={styles} className="box"></div>
+        </button>
+        
     )
 }
 
