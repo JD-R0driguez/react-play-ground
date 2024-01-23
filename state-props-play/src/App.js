2a6c0f9a25529662cfbdaab1e4a9a83fac470a3e
Juan <jd.rodriguez.or@gmail.com>
2024-01-23 10:44:37 -0500

    React state-props practice loaded data

--- state-props-play/src/App.js
+++ state-props-play/src/App.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import boxes from "./boxes"
+import './styles.css'
+
 
 
 export default function App(){
+
+    const [boxesArray, setBoxesArray] = React.useState(boxes) 
+
+
+    /**
+     * Challenge part 1:
+     * 1. Initialize state with the default value of the
+     *    array pulled in from boxes.js
+     * 2. Map over that state array and display each one
+     *    as an empty square (black border, transparent bg color)
+     *    (Don't worry about using the "on" property yet)
+     */
 
     return (
         <div>
