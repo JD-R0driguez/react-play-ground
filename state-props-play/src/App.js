6137d2b9650c8fa62a8bc45a7d010fd3b08e3bdf
Juan <jd.rodriguez.or@gmail.com>
2024-01-23 11:26:50 -0500

    Third challenge using state

--- state-props-play/src/App.js
+++ state-props-play/src/App.js
@@ -13,6 +13,17 @@ export default function App(){
         return <Box id={box.id} state={box.on}/>
     })
 
+    /**
+     * Challenge: Create state controlling whether
+     * this box is "on" or "off". Use the incoming
+     * `props.on` to determine the initial state.
+     * 
+     * Create an event listener so when the box is clicked,
+     * it toggles from "on" to "off".
+     * 
+     * Goal: clicking each box should toggle it on and off.
+     */
+    
     return (
         <div>
             <h1>{[boxArray]}</h1>
