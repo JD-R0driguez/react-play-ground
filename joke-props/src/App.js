698229b30f8de6f93ef571ffe667005bd04db312
Juan <jd.rodriguez.or@gmail.com>
2024-01-25 14:23:02 -0500

    Joke props challenges to practice using state

--- joke-props/src/App.js
+++ joke-props/src/App.js
@@ -8,6 +8,13 @@ export default function App(){
     const jokeList = JokesData.map(joke => (
         <Joke setup = {joke.setup} punch = {joke.punchline}/>
     ));
+
+    /**
+     * Challenge:
+     * - Create state `isShown` (boolean, default to `false`)
+     * - Add a button that toggles the value back and forth
+     */
+
     console.log(jokeList)
     return (
         <div>
--- joke-props/src/Components/Joke.js
+++ joke-props/src/Components/Joke.js
@@ -2,6 +2,13 @@ import React from 'react';
 import './Joke.css'
 
 export default function Joke(props){
+
+    /**
+     * Challenge:
+     * - Create state `isShown` (boolean, default to `false`)
+     * - Add a button that toggles the value back and forth
+     */
+
     return(
         <div className='text'>
             <h3>{props.setup}</h3>
