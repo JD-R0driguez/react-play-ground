8eca31f67bde0ededec8edaa00a9cd2ee9952220
Juan <jd.rodriguez.or@gmail.com>
2024-02-02 18:52:30 -0500

    Updated App components

--- home-page/src/App.jsx
+++ home-page/src/App.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
-
+import Navbar from './components/Navbar'
+import LeftBar from './components/LeftBar'
+import Hero from './components/Hero'
+import About from './components/About'
+import Work from './components/Work'
+import Contact from './components/Contact'
 
 export default function App() {
   return (
     <>
-      <h1>Hello world</h1>
+      <Navbar/>
+      {/* <LeftBar/>
+      <Hero/>
+      <About/>
+      <Work/>
+      <Contact/> */}
     </>
   )
 }
