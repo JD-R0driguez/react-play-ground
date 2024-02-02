8a5dd9d5900b69a61829a05f6f570129872b4730
Juan <jd.rodriguez.or@gmail.com>
2024-02-02 18:53:12 -0500

    Defined NavBar structure

--- home-page/src/components/Navbar.jsx
+++ home-page/src/components/Navbar.jsx
@@ -1,19 +1,27 @@
 import React from "react";
+import './Navbar.css'
 
 
 export default function Navbar(){
     return (
-        <div>
-            <div className="logo--image"></div>
-            <div className="nav--links">
-                <ul>
-                    <li>About</li>
-                    <li>Experience</li>
-                    <li>Projects</li>
-                    <li>Contact</li>
-                </ul>
-                <button className="resume--button">Resume</button>
+        <nav className="nav-bar">
+            <div className="logo-image">
+                <button className="motion-button logo">JR</button>
             </div>
-        </div>
+            <div className="nav-links">
+                <ul>
+                    <li>
+                        <a className="header-anchor-tag" href="">About</a>
+                    </li>
+                    <li>
+                        <a className="header-anchor-tag" href="">Projects</a>
+                    </li>
+                    <li>
+                        <a className="header-anchor-tag" href="">Contact</a>
+                    </li>
+                </ul>
+                <button className="motion-button resume">Resume</button>
+            </div>
+        </nav>
     )
 }
