da3141581e718cbad833b6f628e584ff2beadf90
Juan <jd.rodriguez.or@gmail.com>
2024-02-12 18:16:17 -0500

    Created instances of the TextAnimation component

--- home-page/src/components/Hero.jsx
+++ home-page/src/components/Hero.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import TextAnimation from "./TextAnimation";
+import './Hero.css'
 
 export default function Hero(){
     return (
-        <div>Hero</div>
+        <div className="hero">
+            <div className="back-image fade-img"></div>
+            <section className="hero-title">
+                <TextAnimation text="Hi," />
+                <p className="title-space"><TextAnimation text="I'm" /><TextAnimation text="Juan" /></p>
+                <TextAnimation text="I build things for the web"/>
+            </section>
+        </div>
     )
 }
+
