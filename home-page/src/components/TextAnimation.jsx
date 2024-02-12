e1703e3d79ed0bf6a0c59172b9f12a3c8cc8a30e
Juan <jd.rodriguez.or@gmail.com>
2024-02-12 18:14:15 -0500

    Updated the textAnimation component

--- home-page/src/components/TextAnimation.jsx
+++ home-page/src/components/TextAnimation.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
+import 'animate.css';
 import './TextAnimation.css';
 
-export default function TextAnimation({ text }) {
-    
-    const letters = text.split('')
-    const lettersArray = chars.span(char, index =>(
-        <span key={index} className={`letter ${index < lettersVisible ? 'visible' : ''}`}>
+export default function TextAnimation(props) {
+    console.log(props.text);
+    const lettersArray = props.text.split('').map((char, index) => {
+
+        const charType = char === ' ' ? "space" : "letter";
+        return (<span key={index} className={`${charType} animate__animated animate__bounceInDown`} style={{ animationDelay: `${index * 0.05}s` }}>
             {char}
         </span>
-    ))
+        )
+        });
+
     return (
         <div className="text-animation">
             {lettersArray}
         </div>
     );
 }
-
