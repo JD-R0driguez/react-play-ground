4aae7c0f7aed640ce1ab54aca1b67822bfb5af27
Juan <jd.rodriguez.or@gmail.com>
2024-02-13 12:15:52 -0500

    Completed animation and fixed timing issues for Hero section

--- home-page/src/components/Hero.jsx
+++ home-page/src/components/Hero.jsx
@@ -7,9 +7,12 @@ export default function Hero(){
         <div className="hero">
             <div className="back-image fade-img"></div>
             <section className="hero-title">
-                {/* <TextAnimation text="Hi" style="theme-1"/> */}
                 <TextAnimation text="Hi, I'm Juan" style="theme-1"/>
                 <TextAnimation text="I build things for the web" style="theme-2"/>
+                <h3 className="hero-small animate__animated animate__fadeIn">
+                    I specialize in front-end development and take pride <br/>
+                    in calling The Beer City, Grand Rapids, my home base.
+                </h3>
             </section>
         </div>
     )
