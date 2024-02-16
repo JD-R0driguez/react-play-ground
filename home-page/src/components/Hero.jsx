746928312cc33e0a7f64f83367732903391b8427
Juan <jd.rodriguez.or@gmail.com>
2024-02-15 22:24:12 -0500

    Added styles to about section

--- home-page/src/components/Hero.jsx
+++ home-page/src/components/Hero.jsx
@@ -6,14 +6,16 @@ export default function Hero(){
     return (
         <div className="hero">
             <div className="back-image fade-img"></div>
-            <section className="hero-title">
-                <TextAnimation text="Hi, I'm Juan" style="theme-1"/>
-                <TextAnimation text="I build things for the web" style="theme-2"/>
-                <h3 className="hero-small animate__animated animate__fadeIn">
-                    I specialize in front-end development and take pride <br/>
-                    in calling The Beer City-USA, Grand Rapids, my home base.
-                </h3>
-            </section>
+            <div className="text-container">
+                <section className="hero-title">
+                    <TextAnimation text="Hi, I'm Juan" style="theme-1"/>
+                    <TextAnimation text="I build things for the web" style="theme-2"/>
+                    <h3 className="hero-small animate__animated animate__fadeIn">
+                        I specialize in front-end development and take pride <br/>
+                        in calling The Beer City-USA, Grand Rapids, my home base.
+                    </h3>
+                </section>
+            </div>
         </div>
     )
 }
