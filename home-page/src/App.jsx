2f0e084cc83aab287e10a55b3d60c2167e114ca8
Juan <jd.rodriguez.or@gmail.com>
2024-02-18 19:26:48 -0500

    Added projet component to App.jsx

--- home-page/src/App.jsx
+++ home-page/src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from './components/Navbar'
 import LeftBar from './components/LeftBar'
 import Hero from './components/Hero'
 import About from './components/About'
-import Work from './components/Work'
+import Projects from './components/Projects'
 import Contact from './components/Contact'
 
 export default function App() {
@@ -13,7 +13,7 @@ export default function App() {
       <LeftBar/>
       <Hero/>
       <About/>
-      {/* <Work/> */}
+      <Projects/>
       {/* <Contact/> */}
     </>
   )
