3ec5479b60095aa8337aff9fa52d3ddafbde03e2
Juan <jd.rodriguez.or@gmail.com>
2024-02-18 19:26:05 -0500

    Added projet section layout

--- home-page/src/components/Projects.jsx
+++ home-page/src/components/Projects.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import About from '../assets/about.png'
 import './Projects.css'
 
 
@@ -6,11 +7,94 @@ export default function Projects() {
     return (
         <section className="projects">
             <div className="projects-title">
-                <h4>Get To Know More</h4>
-                <h1>About me</h1>
+                <h4>Check Out Some</h4>
+                <h1>Projects I've Built</h1>
             </div>
-            <div className="projects-split">
-                <h1>Projects here</h1>
+            <div className="section-projects">
+                <div className="project-container-right">
+                    <div className="project-image-container">
+                        <img src={About} alt="project description"/>
+                    </div>
+                    <div className="project-text-container">
+                        <h2 className="project-name">-This Project-</h2>
+                            <div className="text-overlap">
+                                <p>-Project description goes here-
+                                Project description goes here-Project description goes here-
+                                Project description goes here-Project description goes here-
+                                Project description goes here-Project description goes here-
+                                Project description goes here-Project description goes here-
+                                </p>
+                            </div>
+                            <h2 className="tech-stack">-Tech used here-</h2>
+                            <div className="links">
+                                <button className="live-code">-I am a button-</button>
+                                <button className="repo-link">-I take you to github-</button>
+                            </div>
+                    </div>
+                </div>
+                <div className="project-container-left">
+                    <div className="project-image-container">
+                        <img src={About} alt="project description"/>
+                    </div>
+                    <div className="project-text-container">
+                        <h2 className="project-name">-This Project-</h2>
+                            <div className="text-overlap">
+                                <p>-Project description goes here-
+                                Project description goes here-Project description goes here-
+                                Project description goes here-Project description goes here-
+                                Project description goes here-Project description goes here-
+                                Project description goes here-Project description goes here-
+                                </p>
+                            </div>
+                            <h2 className="tech-stack">-Tech used here-</h2>
+                            <div className="links">
+                                <button className="live-code">-I am a button-</button>
+                                <button className="repo-link">-I take you to github-</button>
+                            </div>
+                    </div>
+                </div>
+                <div className="project-container-right">
+                    <div className="project-image-container">
+                        <img src={About} alt="project description"/>
+                    </div>
+                    <div className="project-text-container">
+                        <h2 className="project-name">-This Project-</h2>
+                            <div className="text-overlap">
+                                <p>-Project description goes here-
+                                Project description goes here-Project description goes here-
+                                Project description goes here-Project description goes here-
+                                Project description goes here-Project description goes here-
+                                Project description goes here-Project description goes here-
+                                </p>
+                            </div>
+                            <h2 className="tech-stack">-Tech used here-</h2>
+                            <div className="links">
+                                <button className="live-code">-I am a button-</button>
+                                <button className="repo-link">-I take you to github-</button>
+                            </div>
+                    </div>
+                </div>
+                <div className="project-container-left">
+                    <div className="project-image-container">
+                        <img src={About} alt="project description"/>
+                    </div>
+                    <div className="project-text-container">
+                        <h2 className="project-name">-This Project-</h2>
+                            <div className="text-overlap">
+                                <p>-Project description goes here-
+                                Project description goes here-Project description goes here-
+                                Project description goes here-Project description goes here-
+                                Project description goes here-Project description goes here-
+                                Project description goes here-Project description goes here-
+                                </p>
+                            </div>
+                            <h2 className="tech-stack">-Tech used here-</h2>
+                            <div className="links">
+                                <button className="live-code">-I am a button-</button>
+                                <button className="repo-link">-I take you to github-</button>
+                            </div>
+                    </div>
+                </div>
             </div>
         </section>
     )
