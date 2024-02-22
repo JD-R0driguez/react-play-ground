0a70b3e64cde58a1e18887be5f2c97daa32fefae
Juan <jd.rodriguez.or@gmail.com>
2024-02-22 17:03:45 -0500

    Updated project links

--- home-page/src/components/Projects.jsx
+++ home-page/src/components/Projects.jsx
@@ -53,15 +53,19 @@ export default function Projects() {
                             </div>
                             <h2 className="tech-stack"><pre>JS    Dynamic-Grid-Generation   CSS3/HTML5</pre></h2>
                             <div className="links">
-                                <button className="live-code">Live 
-                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64" width="18" height="18">
-                                        <g id="external-link">
-                                            <path d="M36.026,20.058l-21.092,0c-1.65,0 -2.989,1.339 -2.989,2.989l0,25.964c0,1.65 1.339,2.989 2.989,2.989l26.024,0c1.65,0 2.989,-1.339 2.989,-2.989l0,-20.953l3.999,0l0,21.948c0,3.308 -2.686,5.994 -5.995,5.995l-28.01,0c-3.309,0 -5.995,-2.687 -5.995,-5.995l0,-27.954c0,-3.309 2.686,-5.995 5.995,-5.995l22.085,0l0,4.001Z"/>
-                                            <path d="M55.925,25.32l-4.005,0l0,-10.481l-27.894,27.893l-2.832,-2.832l27.895,-27.895l-10.484,0l0,-4.005l17.318,0l0.002,0.001l0,17.319Z"/>
-                                        </g>
-                                    </svg>
-                                </button>
-                                <button className="repo-link">Source &lt;/&gt;</button>
+                                <a href="https://jd-r0driguez.github.io/etch-a-sketch/">
+                                    <button className="live-code">Live
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64" width="18" height="18">
+                                            <g id="external-link">
+                                                <path d="M36.026,20.058l-21.092,0c-1.65,0 -2.989,1.339 -2.989,2.989l0,25.964c0,1.65 1.339,2.989 2.989,2.989l26.024,0c1.65,0 2.989,-1.339 2.989,-2.989l0,-20.953l3.999,0l0,21.948c0,3.308 -2.686,5.994 -5.995,5.995l-28.01,0c-3.309,0 -5.995,-2.687 -5.995,-5.995l0,-27.954c0,-3.309 2.686,-5.995 5.995,-5.995l22.085,0l0,4.001Z"/>
+                                                <path d="M55.925,25.32l-4.005,0l0,-10.481l-27.894,27.893l-2.832,-2.832l27.895,-27.895l-10.484,0l0,-4.005l17.318,0l0.002,0.001l0,17.319Z"/>
+                                            </g>
+                                        </svg>
+                                    </button>
+                                </a>
+                                <a href="https://github.com/JD-R0driguez/etch-a-sketch">
+                                    <button className="repo-link">Source &lt;/&gt;</button>
+                                </a>
                             </div>
                     </div>
                 </div>
@@ -78,15 +82,19 @@ export default function Projects() {
                             </div>
                             <h2 className="tech-stack"><pre>JS    Event-Handling   CSS3/HTML5</pre></h2>
                             <div className="links">
-                                <button className="live-code">Live 
-                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64" width="18" height="18">
-                                        <g id="external-link">
-                                            <path d="M36.026,20.058l-21.092,0c-1.65,0 -2.989,1.339 -2.989,2.989l0,25.964c0,1.65 1.339,2.989 2.989,2.989l26.024,0c1.65,0 2.989,-1.339 2.989,-2.989l0,-20.953l3.999,0l0,21.948c0,3.308 -2.686,5.994 -5.995,5.995l-28.01,0c-3.309,0 -5.995,-2.687 -5.995,-5.995l0,-27.954c0,-3.309 2.686,-5.995 5.995,-5.995l22.085,0l0,4.001Z"/>
-                                            <path d="M55.925,25.32l-4.005,0l0,-10.481l-27.894,27.893l-2.832,-2.832l27.895,-27.895l-10.484,0l0,-4.005l17.318,0l0.002,0.001l0,17.319Z"/>
-                                        </g>
-                                    </svg>
-                                </button>
-                                <button className="repo-link">Source &lt;/&gt;</button>
+                                <a href="https://jd-r0driguez.github.io/age-calculator-app/">
+                                    <button className="live-code">Live
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64" width="18" height="18">
+                                            <g id="external-link">
+                                                <path d="M36.026,20.058l-21.092,0c-1.65,0 -2.989,1.339 -2.989,2.989l0,25.964c0,1.65 1.339,2.989 2.989,2.989l26.024,0c1.65,0 2.989,-1.339 2.989,-2.989l0,-20.953l3.999,0l0,21.948c0,3.308 -2.686,5.994 -5.995,5.995l-28.01,0c-3.309,0 -5.995,-2.687 -5.995,-5.995l0,-27.954c0,-3.309 2.686,-5.995 5.995,-5.995l22.085,0l0,4.001Z"/>
+                                                <path d="M55.925,25.32l-4.005,0l0,-10.481l-27.894,27.893l-2.832,-2.832l27.895,-27.895l-10.484,0l0,-4.005l17.318,0l0.002,0.001l0,17.319Z"/>
+                                            </g>
+                                        </svg>
+                                    </button>
+                                </a>
+                                <a href="https://github.com/JD-R0driguez/age-calculator-app">
+                                    <button className="repo-link">Source &lt;/&gt;</button>
+                                </a>
                             </div>
                     </div>
                 </div>
@@ -103,15 +111,19 @@ export default function Projects() {
                             </div>
                             <h2 className="tech-stack"><pre>Bootstrap      HTML5      CSS3      jQuery</pre></h2>
                             <div className="links">
-                                <button className="live-code">Live 
-                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64" width="18" height="18">
-                                        <g id="external-link">
-                                            <path d="M36.026,20.058l-21.092,0c-1.65,0 -2.989,1.339 -2.989,2.989l0,25.964c0,1.65 1.339,2.989 2.989,2.989l26.024,0c1.65,0 2.989,-1.339 2.989,-2.989l0,-20.953l3.999,0l0,21.948c0,3.308 -2.686,5.994 -5.995,5.995l-28.01,0c-3.309,0 -5.995,-2.687 -5.995,-5.995l0,-27.954c0,-3.309 2.686,-5.995 5.995,-5.995l22.085,0l0,4.001Z"/>
-                                            <path d="M55.925,25.32l-4.005,0l0,-10.481l-27.894,27.893l-2.832,-2.832l27.895,-27.895l-10.484,0l0,-4.005l17.318,0l0.002,0.001l0,17.319Z"/>
-                                        </g>
-                                    </svg>
-                                </button>
-                                <button className="repo-link">Source &lt;/&gt;</button>
+                                <a href="https://jd-r0driguez.github.io/bootstrap-play-around-site/">
+                                    <button className="live-code">Live
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64" width="18" height="18">
+                                            <g id="external-link">
+                                                <path d="M36.026,20.058l-21.092,0c-1.65,0 -2.989,1.339 -2.989,2.989l0,25.964c0,1.65 1.339,2.989 2.989,2.989l26.024,0c1.65,0 2.989,-1.339 2.989,-2.989l0,-20.953l3.999,0l0,21.948c0,3.308 -2.686,5.994 -5.995,5.995l-28.01,0c-3.309,0 -5.995,-2.687 -5.995,-5.995l0,-27.954c0,-3.309 2.686,-5.995 5.995,-5.995l22.085,0l0,4.001Z"/>
+                                                <path d="M55.925,25.32l-4.005,0l0,-10.481l-27.894,27.893l-2.832,-2.832l27.895,-27.895l-10.484,0l0,-4.005l17.318,0l0.002,0.001l0,17.319Z"/>
+                                            </g>
+                                        </svg>
+                                    </button>
+                                </a>
+                                <a href="https://github.com/JD-R0driguez/bootstrap-play-around-site">
+                                    <button className="repo-link">Source &lt;/&gt;</button>
+                                </a>
                             </div>
                     </div>
                 </div>
@@ -128,15 +140,19 @@ export default function Projects() {
                             </div>
                             <h2 className="tech-stack"><pre>JS    Event-Handling   CSS3/HTML5</pre></h2>
                             <div className="links">
-                                <button className="live-code">Live 
-                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64" width="18" height="18">
-                                        <g id="external-link">
-                                            <path d="M36.026,20.058l-21.092,0c-1.65,0 -2.989,1.339 -2.989,2.989l0,25.964c0,1.65 1.339,2.989 2.989,2.989l26.024,0c1.65,0 2.989,-1.339 2.989,-2.989l0,-20.953l3.999,0l0,21.948c0,3.308 -2.686,5.994 -5.995,5.995l-28.01,0c-3.309,0 -5.995,-2.687 -5.995,-5.995l0,-27.954c0,-3.309 2.686,-5.995 5.995,-5.995l22.085,0l0,4.001Z"/>
-                                            <path d="M55.925,25.32l-4.005,0l0,-10.481l-27.894,27.893l-2.832,-2.832l27.895,-27.895l-10.484,0l0,-4.005l17.318,0l0.002,0.001l0,17.319Z"/>
-                                        </g>
-                                    </svg>
-                                </button>
-                                <button className="repo-link">Source &lt;/&gt;</button>
+                                <a href="https://jd-r0driguez.github.io/simple-drum-javascript/">
+                                    <button className="live-code">Live
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64" width="18" height="18">
+                                            <g id="external-link">
+                                                <path d="M36.026,20.058l-21.092,0c-1.65,0 -2.989,1.339 -2.989,2.989l0,25.964c0,1.65 1.339,2.989 2.989,2.989l26.024,0c1.65,0 2.989,-1.339 2.989,-2.989l0,-20.953l3.999,0l0,21.948c0,3.308 -2.686,5.994 -5.995,5.995l-28.01,0c-3.309,0 -5.995,-2.687 -5.995,-5.995l0,-27.954c0,-3.309 2.686,-5.995 5.995,-5.995l22.085,0l0,4.001Z"/>
+                                                <path d="M55.925,25.32l-4.005,0l0,-10.481l-27.894,27.893l-2.832,-2.832l27.895,-27.895l-10.484,0l0,-4.005l17.318,0l0.002,0.001l0,17.319Z"/>
+                                            </g>
+                                        </svg>
+                                    </button>
+                                </a>
+                                <a href="https://github.com/JD-R0driguez/drum-kit">
+                                    <button className="repo-link">Source &lt;/&gt;</button>
+                                </a>
                             </div>
                     </div>
                 </div>
