04317fc43c1cc9aae25e4b7317e9a1e6423e8e7d
Juan <jd.rodriguez.or@gmail.com>
2024-03-28 12:23:29 -0400

    Aded state to control color mode and passed props to child components

--- light-dark/src/App.jsx
+++ light-dark/src/App.jsx
@@ -3,10 +3,22 @@ import Navbar from "./components/Navbar"
 import Main from "./components/Main"
 
 export default function App() {
+
+    const [colorMode, setColorMode] = React.useState("")
+
+    const colorToggle = () => {
+        setColorMode(prevMode => (
+            prevMode === "" ? "dark" : ""
+        ))
+    }
+
     return (
         <div className="container">
-            <Navbar />
-            <Main />
+            <Navbar 
+                darkMode={colorMode}
+                toggleDarkMode={colorToggle}
+            />
+            <Main darkMode={colorMode}/>
         </div>
     )
 }
--- light-dark/src/components/Navbar.jsx
+++ light-dark/src/components/Navbar.jsx
@@ -1,25 +1,18 @@
 import React from "react"
 import logo from "../images/react-icon-small.png"
 
-export default function Navbar(props) {
+export default function Navbar({darkMode, toggleDarkMode}) {
     return (
-         <nav 
-            className={props.darkMode ? "dark": ""}
-        >
+         <nav className={darkMode ? "dark": ""}>
             <img 
                 className="nav--logo_icon"
                 src={logo}
             />
             <h3 className="nav--logo_text">ReactFacts</h3>
             
-            <div 
-                className="toggler" 
-            >
+            <div className="toggler">
                 <p className="toggler--light">Light</p>
-                <div 
-                    className="toggler--slider"
-                    onClick={props.toggleDarkMode}
-                >
+                <div className="toggler--slider" onClick={()=> toggleDarkMode()}>
                     <div className="toggler--slider--circle"></div>
                 </div>
                 <p className="toggler--dark">Dark</p>
