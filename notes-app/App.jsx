e784f1dfacbc6b08ae94bae15f2eccdd80d75d43
Juan <jd.rodriguez.or@gmail.com>
2024-04-18 14:05:20 -0400

    Initial commit

---
+++ notes-app/App.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+import Sidebar from "./components/Sidebar"
+import Editor from "./components/Editor"
+import Split from "react-split"
+import { nanoid } from "nanoid"
+
+export default function App() {
+    return(
+        <div>Set Up</div>
+    )
+}
