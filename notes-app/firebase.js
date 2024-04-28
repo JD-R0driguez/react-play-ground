1c9f27c04a4cb3e9c3e2d9a3fbd9d9cdf5d00e39
Juan <jd.rodriguez.or@gmail.com>
2024-04-27 20:15:48 -0400

    Added firebase dependencies

---
+++ notes-app/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB54RR7-Xa4FBXKw_DGCzQx1Zl2t6ox6Tw",
+  authDomain: "react-notes-476a6.firebaseapp.com",
+  projectId: "react-notes-476a6",
+  storageBucket: "react-notes-476a6.appspot.com",
+  messagingSenderId: "856362275073",
+  appId: "1:856362275073:web:3fec7ac44b247709688b00"
+};
+
+const app = initializeApp(firebaseConfig);
